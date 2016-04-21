8bb71d43588e94223d29aab536b1d4c43d142bd3
indaso <indaso5@gmail.com>
2016-04-21 00:36:26 -0400

    changed code to use decimate

--- IEEGToolbox/SeniorDesign/getDataForCSV.m
+++ IEEGToolbox/SeniorDesign/getDataForCSV.m
@@ -4,6 +4,8 @@ duration = 600; % in seconds
 session = IEEGSession('I001_P002_D01', 'indaso', 'ind_ieeglogin');
 dataset = session.data;
 
+dec_data = NaN(500,15);
+downData = NaN(100,15);
 numChannels = length(dataset.rawChannels);
 numMinutes = (duration/60);
 numWindows = duration/10;
@@ -47,36 +49,48 @@ for t = 558:numDataWindows
     
     
     % run data through filters
-%     A = high_pass_filter(A, sampleRate);
-    A = low_pass_filter(A, sampleRate);
-    
+    %     A = high_pass_filter(A, sampleRate);
+    %     A = low_pass_filter(A, sampleRate);
+    % decimate each set of 5000 rows ( 450 times )
     numRows = reSampleRate*duration;
-    downSampledData = downsample(A, downSampleRate);
-%     index = (t-1)*numRows + 1;
-    index = ind;
-    allData(index:index + (numRows - 1),:) = downSampledData;
+    loop = duration*sampleRate;
+    for k = 1:5000:loop
+        for j = 1:dn % decimate each channel
+            dec_data(:,j) = decimate(data_clip(k:(k+4999),j),10);
+            downData(:,j) = decimate(dec_data(:,j),5);
+        end
+        index = ind + floor((k/5000))*100;
+        allData(index:index + 99,:) = downData;
+    end
+    
+    %     numRows = reSampleRate*duration;
+    %     downSampledData = downsample(A, downSampleRate);
+    %     index = (t-1)*numRows + 1;
+    %     index = ind;
+    %     allData(index:index + (numRows - 1),:) = downSampledData;
     if(t==numDataWindows)
-        allData(index+60000:end,:) = NaN;
+        allData = allData(1:index + (numRows - 1),:);
+        %         allData(index+60000:end,:) = NaN;
         filename = ['./data/patient1/patient1_' num2str(csvTime) '.mat'];
         csvfile = ['./data/patient1/patient1_' num2str(csvTime) '.csv'];
         save(filename, 'allData','csvTime','csvfile');
-%         writeToCSV(allData, csvTime, filename);
+        %         writeToCSV(allData, csvTime, filename);
         break;
     end
     if(ind == 1140001)
-        if(exist('data/patient1','dir') ~= 7)            
+        if(exist('data/patient1','dir') ~= 7)
             mkdir('data/patient1')
         end
         filename = ['./data/patient1/patient1_' num2str(csvTime) '.mat'];
         csvfile = ['./data/patient1/patient1_' num2str(csvTime) '.csv'];
         save(filename, 'allData','csvTime','csvfile');
-%         writeToCSV(allData, csvTime, filename);
+        %         writeToCSV(allData, csvTime, filename);
         csvTime = csvTime + 1200000*10;
         ind = 1;
     end
     ind = ind + 60000;
     start_time = start_time + duration;
-
+    
     toc
 end
 
